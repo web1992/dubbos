1df32a4be0413d1fe4cf6faffb24d9527d546c30
web1992 <554933654@qq.com>
2019-01-18 11:29:19 +0800

    add [dubbo source]

--- dubbo-source-code/src/main/java/cn/web1992/Protocol$Adaptive.java
+++ dubbo-source-code/src/main/java/cn/web1992/Protocol$Adaptive.java
@@ -1,4 +1,4 @@
-package cn.web1992.dubbo.demo.provider;
+package cn.web1992;
 
 import org.apache.dubbo.common.extension.ExtensionLoader;
 
@@ -12,23 +12,30 @@ public class Protocol$Adaptive implements org.apache.dubbo.rpc.Protocol {
     }
 
     public org.apache.dubbo.rpc.Exporter export(org.apache.dubbo.rpc.Invoker arg0) throws org.apache.dubbo.rpc.RpcException {
-        if (arg0 == null) throw new IllegalArgumentException("org.apache.dubbo.rpc.Invoker argument == null");
-        if (arg0.getUrl() == null)
+        if (arg0 == null) {
+            throw new IllegalArgumentException("org.apache.dubbo.rpc.Invoker argument == null");
+        }
+        if (arg0.getUrl() == null) {
             throw new IllegalArgumentException("org.apache.dubbo.rpc.Invoker argument getUrl() == null");
+        }
         org.apache.dubbo.common.URL url = arg0.getUrl();
         String extName = (url.getProtocol() == null ? "dubbo" : url.getProtocol());
-        if (extName == null)
+        if (extName == null) {
             throw new IllegalStateException("Fail to get extension(org.apache.dubbo.rpc.Protocol) name from url(" + url.toString() + ") use keys([protocol])");
+        }
         org.apache.dubbo.rpc.Protocol extension = (org.apache.dubbo.rpc.Protocol) ExtensionLoader.getExtensionLoader(org.apache.dubbo.rpc.Protocol.class).getExtension(extName);
         return extension.export(arg0);
     }
 
     public org.apache.dubbo.rpc.Invoker refer(java.lang.Class arg0, org.apache.dubbo.common.URL arg1) throws org.apache.dubbo.rpc.RpcException {
-        if (arg1 == null) throw new IllegalArgumentException("url == null");
+        if (arg1 == null) {
+            throw new IllegalArgumentException("url == null");
+        }
         org.apache.dubbo.common.URL url = arg1;
         String extName = (url.getProtocol() == null ? "dubbo" : url.getProtocol());
-        if (extName == null)
+        if (extName == null) {
             throw new IllegalStateException("Fail to get extension(org.apache.dubbo.rpc.Protocol) name from url(" + url.toString() + ") use keys([protocol])");
+        }
         org.apache.dubbo.rpc.Protocol extension = (org.apache.dubbo.rpc.Protocol) ExtensionLoader.getExtensionLoader(org.apache.dubbo.rpc.Protocol.class).getExtension(extName);
         return extension.refer(arg0, arg1);
     }
