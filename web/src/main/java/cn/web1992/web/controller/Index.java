b45bdaedc8ae2db1371ece65501c042ec42eb23c
erbao.wang <554933654@qq.com>
2019-09-15 20:29:10 +0800

    add hello service

--- web/src/main/java/cn/web1992/web/controller/Index.java
+++ web/src/main/java/cn/web1992/web/controller/Index.java
@@ -1,14 +1,22 @@
 package cn.web1992.web.controller;
 
+import cn.web1992.web.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Index {
 
+    private HelloService helloService;
+
+    @Autowired
+    public void setHelloService(HelloService helloService) {
+        this.helloService = helloService;
+    }
 
     @RequestMapping(name = "index", path = "/")
     public String index() {
-        return "welcome !";
+        return "welcome !" + helloService.say();
     }
 }
--- web/src/main/java/cn/web1992/web/service/HelloServiceImpl.java
+++ web/src/main/java/cn/web1992/web/service/HelloServiceImpl.java
@@ -1,22 +1,25 @@
 package cn.web1992.web.service;
 
+import org.springframework.stereotype.Service;
+
 /**
  * @author web1992
  */
+@Service("HelloService")
 public class HelloServiceImpl implements HelloService {
 
     @Override
     public String nihao() {
-        return "你好！";
+        return " 你好！";
     }
 
     @Override
     public String say() {
-        return "CCC Hello from HelloService B";
+        return " Hello from HelloService";
     }
 
     @Override
     public String bye() {
-        return "DDD Bye bye from HelloService";
+        return " Bye bye from HelloService";
     }
 }
