bffd1d7478feb6a2f6b3e7a1c77f2ec81d912965
web1992 <554933654@qq.com>
2022-09-16 14:30:16 +0800

    fix [dubbo version error]

--- dubbo-source-code/src/main/java/cn/web1992/Validation$Adaptive.java
+++ dubbo-source-code/src/main/java/cn/web1992/Validation$Adaptive.java
@@ -1,18 +1,18 @@
-package cn.web1992;
-
-import org.apache.dubbo.common.extension.ExtensionLoader;
-
-public class Validation$Adaptive implements org.apache.dubbo.validation.Validation {
-    public org.apache.dubbo.validation.Validator getValidator(org.apache.dubbo.common.URL arg0) {
-        if (arg0 == null) {
-            throw new IllegalArgumentException("url == null");
-        }
-        org.apache.dubbo.common.URL url = arg0;
-        String extName = url.getParameter("validation", "jvalidation");
-        if (extName == null) {
-            throw new IllegalStateException("Fail to get extension(org.apache.dubbo.validation.Validation) name from url(" + url.toString() + ") use keys([validation])");
-        }
-        org.apache.dubbo.validation.Validation extension = (org.apache.dubbo.validation.Validation) ExtensionLoader.getExtensionLoader(org.apache.dubbo.validation.Validation.class).getExtension(extName);
-        return extension.getValidator(arg0);
-    }
-}
+//package cn.web1992;
+//
+//import org.apache.dubbo.common.extension.ExtensionLoader;
+//
+//public class Validation$Adaptive implements org.apache.dubbo.validation.Validation {
+//    public org.apache.dubbo.validation.Validator getValidator(org.apache.dubbo.common.URL arg0) {
+//        if (arg0 == null) {
+//            throw new IllegalArgumentException("url == null");
+//        }
+//        org.apache.dubbo.common.URL url = arg0;
+//        String extName = url.getParameter("validation", "jvalidation");
+//        if (extName == null) {
+//            throw new IllegalStateException("Fail to get extension(org.apache.dubbo.validation.Validation) name from url(" + url.toString() + ") use keys([validation])");
+//        }
+//        org.apache.dubbo.validation.Validation extension = (org.apache.dubbo.validation.Validation) ExtensionLoader.getExtensionLoader(org.apache.dubbo.validation.Validation.class).getExtension(extName);
+//        return extension.getValidator(arg0);
+//    }
+//}
--- readme.md
+++ readme.md
@@ -6,7 +6,7 @@ some demo of dubbo
 
 This project have 2 profiles in `pom.xml`
 
-- dubbo-2.7.x -> 2.7.2 (default active)
+- dubbo-2.7.x -> 2.7.17 (default active)
 - dubbo-2.6.x -> 2.6.5
 
 ```sh
